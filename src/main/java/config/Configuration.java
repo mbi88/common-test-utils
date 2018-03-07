8af52c008b4ac052a4a0766749578643a5ea6f69
mb <>
2018-03-07 17:41:54 +0200

    read env var method

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -2,12 +2,20 @@ package config;
 
 import org.json.JSONObject;
 
+import java.util.Objects;
+
 import static serializer.JsonDeserializer.getJsonFromFile;
 
 /**
  * Tests configurations.
  */
 public interface Configuration {
+
+    default void readVar(final String varName) {
+        System.out.println("Reading env var " + varName + ": ");
+        String result = Objects.isNull(System.getenv(varName)) ? System.getenv(varName) : "null";
+        System.out.println(result);
+    }
 
     /**
      * Available environments.
