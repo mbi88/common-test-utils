ddad644fb2c380081fbc2b3c1e7217d0c6e643da
mb <mbinkovskiy@gmail.com>
2018-04-12 11:46:57 +0300

    read ssm parameters

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -1,10 +1,17 @@
 package config;
 
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParametersRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParametersResult;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,6 +35,42 @@ public interface Configuration {
         log.info(result + "\n");
 
         return result;
+    }
+
+    /**
+     * Reads parameters from Amazon EC2 Systems Manager (SSM).
+     *
+     * @param paramNames parameters names.
+     * @return parameters and its values.
+     */
+    default Map<String, String> readSsmParameters(final String... paramNames) {
+        final AWSSimpleSystemsManagement client = AWSSimpleSystemsManagementClientBuilder.defaultClient();
+        final GetParametersRequest request = new GetParametersRequest();
+        request.withNames(paramNames).setWithDecryption(true);
+        final GetParametersResult result = client.getParameters(request);
+
+        final Map<String, String> store = new HashMap<>();
+        result.getParameters().forEach(parameter -> store.put(parameter.getName(), parameter.getValue()));
+
+        return store;
+    }
+
+    /**
+     * Reads parameter from Amazon EC2 Systems Manager (SSM).
+     *
+     * @param paramName parameters names.
+     * @return parameters and its values.
+     */
+    default String readSsmParameter(final String paramName) {
+        final AWSSimpleSystemsManagement client = AWSSimpleSystemsManagementClientBuilder.defaultClient();
+        final GetParametersRequest request = new GetParametersRequest();
+        request.withNames(paramName).setWithDecryption(true);
+        final GetParametersResult result = client.getParameters(request);
+
+        final AtomicReference<String> value = new AtomicReference<>();
+        result.getParameters().forEach(parameter -> value.set(parameter.getValue()));
+
+        return value.toString();
     }
 
     /**
