6cf94de95c91f66b7ff1cf9422fb7c283bfe32f2
mb <mbinkovskiy@gmail.com>
2018-09-07 16:49:16 +0300

    test latest dependency version

---
+++ src/main/java/endpoint/A.java
@@ -0,0 +1,26 @@
+package endpoint;
+
+/**
+ * Created by mbi on 10/6/16.
+ */
+public interface A {
+
+    /**
+     * Assure access is granted with valid auth token.
+     * "allowAll": true
+     * Expected status code: 2XX
+     */
+    void testCanGetAccess();
+
+    /**
+     * Assure access is denied if token is invalid.
+     * Expected status code: 401
+     */
+    void testCantGetAccessWithInvalidToken();
+
+    /**
+     * Assure no access if request performed without Authorization header.
+     * Expected status code: 401
+     */
+    void testCantGetAccessWithoutAuth();
+}
