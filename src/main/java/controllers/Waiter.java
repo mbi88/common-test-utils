7dbdc0bb4ccac93261de2987ec5856bc0c638207
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2019-02-26 17:08:16 +0200

    implement waiter 2.0

--- src/main/java/controllers/Waiter.java
+++ src/main/java/controllers/Waiter.java
@@ -2,9 +2,6 @@ package controllers;
 
 import org.joda.time.DateTime;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -12,93 +9,64 @@ import java.util.function.Supplier;
 /**
  * Waiting for special object condition. Condition is checked every 1 sec.
  *
- * @param <T>
+ * @param <T> type of result of function to execute.
  */
 public final class Waiter<T> {
 
-	/**
-	 * Time in seconds to be spent on waiting of condition.
-	 */
-	private final int waitingTime;
+    /**
+     * Time in seconds to be spent on waiting of condition.
+     */
+    private final int waitingTime;
 
-	/**
-	 *
-	 */
-	private final Supplier<T> function;
+    /**
+     * Function to execute.
+     */
+    private final Supplier<T> function;
 
-	/**
-	 *
-	 */
-	private final Function<T, String> result;
+    /**
+     * Function execution result to be thrown if expected condition will not be met.
+     */
+    private final Function<T, String> result;
 
-	private boolean printResultWhileWait = false;
+    /**
+     * @param function    function to execute.
+     * @param result      string representation of function result to be used in exception if occurs.
+     * @param waitingTime how many seconds waiter will wait until throw exception.
+     */
+    public Waiter(final Supplier<T> function, final Function<T, String> result, final int waitingTime) {
+        this.function = function;
+        this.result = result;
+        this.waitingTime = waitingTime;
+    }
 
-	/**
-	 * Waiter constructor.
-	 *
-	 * @param function    d.
-	 * @param result      string representation of function result to be used if exception occurs.
-	 * @param waitingTime how many seconds waiter will wait until throw exception.
-	 */
-	public Waiter(final Supplier<T> function, final Function<T, String> result, final int waitingTime) {
-		this.function = function;
-		this.result = result;
-		this.waitingTime = waitingTime;
-	}
+    /**
+     * @param expectedCondition condition.
+     * @return result response.
+     * @throws Error if expected condition not met during waiting time.
+     */
+    @SuppressWarnings("PMD.AvoidThrowingRawExceptionTypes")
+    public T waitCondition(final Predicate<T> expectedCondition) {
+        final long startTime = DateTime.now().getMillis();
+        final long endTime = startTime + waitingTime * 1000L;
+        boolean timeExceeded = false;
+        T response = function.get();
 
-	/**
-	 * Waiter constructor.
-	 *
-	 * @param function    d.
-	 * @param result      string representation of function result to be used if exception occurs.
-	 * @param waitingTime how many seconds waiter will wait until throw exception.
-	 */
-	public Waiter(final Supplier<T> function, final Function<T, String> result, final int waitingTime, final boolean printResultWhileWait) {
-		this.function = function;
-		this.result = result;
-		this.waitingTime = waitingTime;
-		this.printResultWhileWait = printResultWhileWait;
-	}
+        while (!timeExceeded && !expectedCondition.test(response)) {
+            timeExceeded = DateTime.now().getMillis() >= endTime;
+            response = function.get();
 
-	/**
-	 * @param expectedCondition condition.
-	 * @return result response.
-	 */
-	@SuppressWarnings("PMD.AvoidThrowingRawExceptionTypes")
-	public T waitCondition(final Predicate<T> expectedCondition) {
-		T r = function.get();
-		final long startTime = DateTime.now().getMillis();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {
+                // Ignored
+            }
+        }
 
-		while (!expectedCondition.test(r)) {
-			if (!waiting(startTime)) {
-				throw new Error(String.format(
-						"Expected conditions not met. Max waiting time exceeded%n%nResult: %s%n",
-						result.apply(r)));
-			}
+        if (timeExceeded) {
+            throw new Error(String.format("Expected condition not met. Max waiting time exceeded%n%nResult: %s%n",
+                    result.apply(response)));
+        }
 
-			r = function.get();
-		}
-
-		return r;
-	}
-
-	/**
-	 * Whether waiter should sleep or throw exception.
-	 * Sleep 1 second.
-	 *
-	 * @param startTime time before first request.
-	 */
-	private boolean waiting(final long startTime) {
-		ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-		scheduledExecutorService.schedule(() -> {
-					String res;
-					res = printResultWhileWait ? result.apply(function.get()) : "23";
-					System.out.println(res);
-					return res;
-				},
-				1,
-				TimeUnit.SECONDS);
-
-		return (startTime + waitingTime * 1000L) > DateTime.now().getMillis();
-	}
+        return response;
+    }
 }
--- src/test/java/tests/ControllersTest.java
+++ src/test/java/tests/ControllersTest.java
@@ -1,10 +1,7 @@
 package tests;
 
 import com.mbi.request.RequestBuilder;
-import controllers.Controller;
-import controllers.Creatable;
-import controllers.QueryParameter;
-import controllers.Waiter;
+import controllers.*;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBodyData;
 import org.testng.annotations.Test;
@@ -17,118 +14,118 @@ import static testcase.BaseTestCase.toJson;
 
 public class ControllersTest {
 
-	@Test
-	public void testCanAddSameParameters() {
-		QueryParameter parameter = new QueryParameter();
-		parameter.addParameter("q", 1);
-		parameter.addParameter("q", 1);
+    @Test
+    public void testCanAddSameParameters() {
+        QueryParameter parameter = new QueryParameter();
+        parameter.addParameter("q", 1);
+        parameter.addParameter("q", 1);
 
-		assertEquals(parameter.getParametersString(), "?q=1&q=1");
-	}
+        assertEquals(parameter.getParametersString(), "?q=1&q=1");
+    }
 
-	@Test
-	public void testCanSeveralParameters() {
-		QueryParameter parameter = new QueryParameter();
-		parameter.addParameter("q", 1);
-		parameter.addParameter("w", 2);
+    @Test
+    public void testCanSeveralParameters() {
+        QueryParameter parameter = new QueryParameter();
+        parameter.addParameter("q", 1);
+        parameter.addParameter("w", 2);
 
-		assertEquals(parameter.getParametersString(), "?q=1&w=2");
-	}
+        assertEquals(parameter.getParametersString(), "?q=1&w=2");
+    }
 
-	@Test
-	public void testAddParametersViaConstructor() {
-		QueryParameter parameter = new QueryParameter("q", 1);
-		parameter.addParameter("w", 2);
+    @Test
+    public void testAddParametersViaConstructor() {
+        QueryParameter parameter = new QueryParameter("q", 1);
+        parameter.addParameter("w", 2);
 
-		assertEquals(parameter.getParametersString(), "?q=1&w=2");
-	}
+        assertEquals(parameter.getParametersString(), "?q=1&w=2");
+    }
 
-	@Test
-	public void testWaiterIfConditionIsMet() {
-		Waiter<Response> waiter = new Waiter<>(() -> new RequestBuilder().get("https://www.google.com.ua/"),
-				ResponseBodyData::asString,
-				10);
-		Predicate<Response> predicate = response -> response.statusCode() == 200;
+    @Test
+    public void testWaiterIfConditionIsMet() {
+        Waiter<Response> waiter = new Waiter<>(() -> new RequestBuilder().get("https://www.google.com.ua/"),
+                ResponseBodyData::asString,
+                10);
+        Predicate<Response> predicate = response -> response.statusCode() == 200;
 
-		waiter.waitCondition(predicate);
-	}
+        waiter.waitCondition(predicate);
+    }
 
-	@Test
-	public void testWaiterIfConditionIsNoMet() {
-		Waiter<Response> waiter = new Waiter<>(() -> new RequestBuilder().get("http://www.mocky.io/v2/5ab8a4952c00005700186093"),
-				ResponseBodyData::asString,
-				10, false);
-		Predicate<Response> predicate = response -> response.statusCode() == 20;
+    @Test
+    public void testWaiterIfConditionIsNoMet() {
+        Waiter<Response> waiter = new Waiter<>(() -> new RequestBuilder().get("http://www.mocky.io/v2/5ab8a4952c00005700186093"),
+                ResponseBodyData::asString,
+                10);
+        Predicate<Response> predicate = response -> response.statusCode() == 20;
 
-		try {
-			waiter.waitCondition(predicate);
-		} catch (Throwable t) {
-			assertTrue(t.getMessage().contains("Max waiting time exceeded"));
-		}
-	}
+        try {
+            waiter.waitCondition(predicate);
+        } catch (Throwable t) {
+            assertTrue(t.getMessage().contains("Max waiting time exceeded"));
+        }
+    }
 
-	@Test
-	public void testWithIncorrectId() {
-		class TestClass extends Controller<TestClass> {
-		}
-		TestClass testClass = new TestClass();
+    @Test
+    public void testWithIncorrectId() {
+        class TestClass extends Controller<TestClass> {
+        }
+        TestClass testClass = new TestClass();
 
-		assertTrue(testClass.withIncorrectId().getId().toString()
-				.matches("^([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})$"));
-	}
+        assertTrue(testClass.withIncorrectId().getId().toString()
+                .matches("^([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})$"));
+    }
 
-	@Test
-	public void testWithoutId() {
-		TestClass testClass = new TestClass();
+    @Test
+    public void testWithoutId() {
+        TestClass testClass = new TestClass();
 
-		assertEquals(testClass.withoutId().getId().toString().length(), 0);
-	}
+        assertEquals(testClass.withoutId().getId().toString().length(), 0);
+    }
 
-	@Test
-	public void testSettingIdViaConstructor() {
-		TestClass testClass = new TestClass(1);
+    @Test
+    public void testSettingIdViaConstructor() {
+        TestClass testClass = new TestClass(1);
 
-		assertEquals(testClass.getId(), 1);
-	}
+        assertEquals(testClass.getId(), 1);
+    }
 
-	@Test
-	public void testErrorMessageOnNullId() {
-		TestClass testClass = new TestClass(null);
+    @Test
+    public void testErrorMessageOnNullId() {
+        TestClass testClass = new TestClass(null);
 
-		boolean passed;
-		try {
-			testClass.getId();
-			passed = true;
-		} catch (NullPointerException ex) {
-			passed = false;
-			assertEquals(ex.getMessage(), "TestClass: Object id is not initialized");
-		}
-		assertFalse(passed);
-	}
+        boolean passed;
+        try {
+            testClass.getId();
+            passed = true;
+        } catch (NullPointerException ex) {
+            passed = false;
+            assertEquals(ex.getMessage(), "TestClass: Object id is not initialized");
+        }
+        assertFalse(passed);
+    }
 
-	@Test
-	public void testExtractingIdFromResponse() {
-		TestClass testClass = new TestClass();
-		testClass.getResponse();
+    @Test
+    public void testExtractingIdFromResponse() {
+        TestClass testClass = new TestClass();
+        testClass.getResponse();
 
-		assertEquals(testClass.getId(), 1);
-	}
+        assertEquals(testClass.getId(), 1);
+    }
 
-	class TestClass extends Controller<TestClass> implements Creatable {
-		private final Function<Response, Integer> getIdFunction = response -> toJson(response).optInt("a");
+    class TestClass extends Controller<TestClass> implements Creatable {
+        private final Function<Response, Integer> getIdFunction = response -> toJson(response).optInt("a");
 
-		TestClass() {
-		}
+        TestClass() {
+        }
 
-		TestClass(Integer id) {
-			super(id);
-		}
+        TestClass(Integer id) {
+            super(id);
+        }
 
-		Response getResponse() {
-			Response response = http.get("http://www.mocky.io/v2/5ab8a4952c00005700186093");
-			setId(extractId(response, getIdFunction));
+        Response getResponse() {
+            Response response = http.get("http://www.mocky.io/v2/5ab8a4952c00005700186093");
+            setId(extractId(response, getIdFunction));
 
-			return response;
-		}
-	}
+            return response;
+        }
+    }
 }
